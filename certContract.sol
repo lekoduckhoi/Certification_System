aa014fc73432cfef376f5e472d5c1e9b4bdc0824
lekoduckhoi <63868028+lekoduckhoi@users.noreply.github.com>
2021-06-29 21:28:35 +0700

    Update certContract.sol

--- certContract.sol
+++ certContract.sol
@@ -5,91 +5,94 @@ contract Certificate_Factory {
     // set owner's address
     address owner;
     uint contractCreateTime;
+    
+    // Issuer
+    mapping(address => bool) isIssuer;
+    
+    // list of cert address
+    mapping(address => bool) addressExist;
+    address[] certificateAddresses;
+    uint certCounter;
+    
+    // track certificate Address
+    mapping (string => address) idToAddress;
+    mapping (string => string) idToIpfs;
+    
+    // verify if ipfsHash exist
+    mapping(string => bool) ipfsHashExist;
+    
+    // events
+    event Add(string indexed _name, string indexed _id, string indexed _ipfsHash, uint _time);
+    event Remove(string indexed _id);
+    
     constructor() {
         owner = msg.sender;
         contractCreateTime = block.timestamp;
     }
     
     // Program details 
-    string name = "BLOCKCHAIN MATHEMATICS AND COMPUTING";
-    string host = "VIASM (Vietnam Institute for Advanced Study in Mathematics)";
-    
+    string private name = "BLOCKCHAIN MATHEMATICS AND COMPUTING (25-27/6/2021, 3-4/7/2021)";
+    string private host = "VIASM (Vietnam Institute for Advanced Study in Mathematics)";
+
     function viewProgramName() public view returns(string memory) {
         return name;
     }
-    
     function viewProgramHost() public view returns(string memory) {
         return host;
     }
-    
-    function viewProgramHostedTime() public view returns(uint) {
+    function viewProgramCreationTime() public view returns(uint) {
         return contractCreateTime;
     }
-    
-    // list of cert address
-    mapping(address => bool) addressExist;
-    address[] certificateAddresses;
-    uint certCounter;
+    function viewAllCertAddresses() public view returns(address[] memory) {
+        return certificateAddresses;
+    }
+    function viewNumberOfCertificates() public view returns(uint) {
+        return certCounter;
+    }
+    function viewCertAddressById(string memory _id) public view returns(address) {
+        return idToAddress[_id];
+    }
     function verifyAddress(address _address) public view returns(bool) {
         return(addressExist[_address]);
     }
-    function viewAllCertAddress() public view returns(address[] memory) {
-        return certificateAddresses;
+    function verifyIpfsHash(string memory _ipfsHash) public view returns(bool) {
+        return(ipfsHashExist[_ipfsHash]);
     }
-    function viewNumberOfCert() public view returns(uint) {
-        return certCounter;
-    }
-    
-    // track certificate Address
-    mapping (uint => address) idToAddress;
-    
-    function viewCertAddressById(uint _id) public view returns(address) {
-        return idToAddress[_id];
+    //set, remove issuer function
+    function AddIssuer(address _newIssuer) public {
+        require(msg.sender == owner || isIssuer[msg.sender] == true, "Must be owner or Issuer");
+        isIssuer[_newIssuer] = true;
     }
     
-    // add new certificate contract
-    event Add(string indexed _name, uint indexed _id, string indexed _ipfsHash, uint _time);
-    
-    function addCertificate(string memory _name, uint _id, string memory _ipfsHash) public {
-        require(msg.sender == owner, 'Must be Owner to add new Certificate');
+    function addCertificate(string memory _name, string memory _id, string memory _ipfsHash) public {
+        require(msg.sender == owner || isIssuer[msg.sender] == true, "Must be owner or Issuer");
         certCounter++;
         Certificate newCertificate = new Certificate(_name, _id, _ipfsHash);
         idToAddress[_id] = address(newCertificate);
+        idToIpfs[_id]= _ipfsHash;
         certificateAddresses.push(address(newCertificate));
         ipfsHashExist[_ipfsHash] = true;
         addressExist[address(newCertificate)] = true;
+        
         emit Add(_name, _id, _ipfsHash, block.timestamp);
     }
     
-    // remove cert for id
-    event Remove(uint indexed _id);
-    
-    function remove(uint _id) public {
-        require(msg.sender == owner);
+    function remove(string memory _id) private {
+        require(msg.sender == owner || isIssuer[msg.sender] == true, "Must be owner or Issuer");
+        certCounter--;
         delete idToAddress[_id];
         delete addressExist[viewCertAddressById(_id)];
+        delete idToIpfs[_id];
+        delete ipfsHashExist[idToIpfs[_id]];
         emit Remove(_id);
-    }
-    
-    // verify if ipfsHash exist
-    mapping(string => bool) ipfsHashExist;
-    
-    function verify(string memory _ipfsHash) public view returns(bool) {
-        return(ipfsHashExist[_ipfsHash]);
-    }
-    
-    function deleteIpfsHash(string memory _ipfsHash) public {
-        require(ipfsHashExist[_ipfsHash] == true, "Ipfs doesn't exist");
-        require(msg.sender == owner);
-        ipfsHashExist[_ipfsHash] = false;
     }
 }
 
 contract Certificate {
     // Certificate details;
-    event Details(string name, uint id, string ipfsHash, uint createdTime, address factoryContractAddress);
+    event Details(string name, string id, string ipfsHash, uint createdTime, address factoryContractAddress);
 
-    constructor(string memory _name, uint _id, string memory _ipfsHash) {
+    constructor(string memory _name, string memory _id, string memory _ipfsHash) {
         emit Details(_name, _id, _ipfsHash, block.timestamp, msg.sender);
     }
 }
