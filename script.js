92cef49e42cc2d2adf477e008dcffd968cc5c302
lekoduckhoi <63868028+lekoduckhoi@users.noreply.github.com>
2021-06-29 21:24:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -58,9 +58,9 @@ const certFacABI = [
 			},
 			{
 				"indexed": true,
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "_id",
-				"type": "uint256"
+				"type": "string"
 			},
 			{
 				"indexed": true,
@@ -79,6 +79,32 @@ const certFacABI = [
 		"type": "event"
 	},
 	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "string",
+				"name": "_id",
+				"type": "string"
+			}
+		],
+		"name": "Remove",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_newIssuer",
+				"type": "address"
+			}
+		],
+		"name": "AddIssuer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"inputs": [
 			{
 				"internalType": "string",
@@ -86,9 +112,9 @@ const certFacABI = [
 				"type": "string"
 			},
 			{
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "_id",
-				"type": "uint256"
+				"type": "string"
 			},
 			{
 				"internalType": "string",
@@ -99,64 +125,6 @@ const certFacABI = [
 		"name": "addCertificate",
 		"outputs": [],
 		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ipfsHash",
-				"type": "string"
-			}
-		],
-		"name": "deleteIpfsHash",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_id",
-				"type": "uint256"
-			}
-		],
-		"name": "remove",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "uint256",
-				"name": "_id",
-				"type": "uint256"
-			}
-		],
-		"name": "Remove",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_ipfsHash",
-				"type": "string"
-			}
-		],
-		"name": "verify",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -179,8 +147,27 @@ const certFacABI = [
 		"type": "function"
 	},
 	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_ipfsHash",
+				"type": "string"
+			}
+		],
+		"name": "verifyIpfsHash",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
 		"inputs": [],
-		"name": "viewAllCertAddress",
+		"name": "viewAllCertAddresses",
 		"outputs": [
 			{
 				"internalType": "address[]",
@@ -194,9 +181,9 @@ const certFacABI = [
 	{
 		"inputs": [
 			{
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "_id",
-				"type": "uint256"
+				"type": "string"
 			}
 		],
 		"name": "viewCertAddressById",
@@ -212,7 +199,20 @@ const certFacABI = [
 	},
 	{
 		"inputs": [],
-		"name": "viewNumberOfCert",
+		"name": "viewNumberOfCertificates",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "viewProgramCreationTime",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -238,19 +238,6 @@ const certFacABI = [
 	},
 	{
 		"inputs": [],
-		"name": "viewProgramHostedTime",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "viewProgramName",
 		"outputs": [
 			{
@@ -263,7 +250,7 @@ const certFacABI = [
 		"type": "function"
 	}
 ]
-const certFacAddress = "0x29F298926b9CE9f189D70d92A3Bbc2Ce9B68b35A"
+const certFacAddress = "0xA73E9a3EdDDb3A0D207e24bE47cc299f21e3cc41"
 const certFacContract = new web3.eth.Contract(certFacABI, certFacAddress)
 
 const certABI = [
@@ -275,9 +262,9 @@ const certABI = [
 				"type": "string"
 			},
 			{
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "_id",
-				"type": "uint256"
+				"type": "string"
 			},
 			{
 				"internalType": "string",
@@ -299,9 +286,9 @@ const certABI = [
 			},
 			{
 				"indexed": false,
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "id",
-				"type": "uint256"
+				"type": "string"
 			},
 			{
 				"indexed": false,
@@ -331,14 +318,44 @@ var certAddress
 let submit = document.getElementById('submit')
 let link_downloadable = document.getElementById('link__downloadable')
 submit.addEventListener('click',()=>{
-  let address = $("#input").val()
+	cert_found.classList.add('hidden')
+	let address = $("#input").val()
   if(!web3.utils.isAddress(address)) {
-    alert("Please enter a valid address")
+	certFacContract.methods.viewCertAddressById(SHA1(address)).call((err, res2) => {
+		if(res2 == "0x0000000000000000000000000000000000000000"){
+			alert("Please enter a valid citizen identification number")
+		} else {
+			cert_found.classList.remove('hidden')
+			$("#certImg").attr("src", "./image/loadd.gif")
+			$("#abcd").html("CERTIFICATE DETAILS")
+			$("#issuedBy").html("Issued By: Vietnam Institute for Advanced Study in Mathematics (VIASM).")
+			$("#course").html("Course: BLOCKCHAIN MATHEMATICS AND COMPUTING")
+			$("#date").html("Date: 04/07/2021")
+			console.log("smt")
+			console.log($("#abcd").val())
+			certAddress = res2
+			var certContract = new web3.eth.Contract(certABI, certAddress)
+			certContract.getPastEvents(
+				'Details',
+				{ 
+					fromBlock: 10520249,
+					toBlock: 'latest'
+				},
+				(err, result) => { 
+					console.log(result)
+					$("#issuedTo").html("Issued To: " + result[0].returnValues.name)
+					$("#link__downloadable").attr("href","https://gateway.pinata.cloud/ipfs/"+result[0].returnValues.ipfsHash)
+			 	  	$("#link__downloadable").html("https://gateway.pinata.cloud/ipfs/"+result[0].returnValues.ipfsHash)
+				    $("#certImg").attr("src", "https://gateway.pinata.cloud/ipfs/"+result[0].returnValues.ipfsHash)
+				}
+			)
+		}
+	})
   } else {
     $("#certImg").attr("src", "./image/loadd.gif")
 	cert_found.classList.remove('hidden')
     certFacContract.methods.verifyAddress(address).call((err, result) => {
-      if(result == 0) {
+      if(result == false) {
         cert_found.classList.remove('hidden')
         link_downloadable.innerHTML = "No link found"
         $("#certImg").attr("src", "./image/defaultCert.jpg")
@@ -428,4 +445,22 @@ let check = false // sửa dòng này
             });
         }
     });   
+
+// SHA1 function
+function SHA1(msg){function rotate_left(n,s){var t4=(n<<s)|(n>>>(32-s));return t4;};function lsb_hex(val){var str='';var i;var vh;var vl;for(i=0;i<=6;i+=2){vh=(val>>>(i*4+4))&0x0f;vl=(val>>>(i*4))&0x0f;str+=vh.toString(16)+vl.toString(16);}
+return str;};function cvt_hex(val){var str='';var i;var v;for(i=7;i>=0;i--){v=(val>>>(i*4))&0x0f;str+=v.toString(16);}
+return str;};function Utf8Encode(string){string=string.replace(/\r\n/g,'\n');var utftext='';for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}
+else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}
+else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}
+return utftext;};var blockstart;var i,j;var W=new Array(80);var H0=0x67452301;var H1=0xEFCDAB89;var H2=0x98BADCFE;var H3=0x10325476;var H4=0xC3D2E1F0;var A,B,C,D,E;var temp;msg=Utf8Encode(msg);var msg_len=msg.length;var word_array=new Array();for(i=0;i<msg_len-3;i+=4){j=msg.charCodeAt(i)<<24|msg.charCodeAt(i+1)<<16|msg.charCodeAt(i+2)<<8|msg.charCodeAt(i+3);word_array.push(j);}
+switch(msg_len % 4){case 0:i=0x080000000;break;case 1:i=msg.charCodeAt(msg_len-1)<<24|0x0800000;break;case 2:i=msg.charCodeAt(msg_len-2)<<24|msg.charCodeAt(msg_len-1)<<16|0x08000;break;case 3:i=msg.charCodeAt(msg_len-3)<<24|msg.charCodeAt(msg_len-2)<<16|msg.charCodeAt(msg_len-1)<<8|0x80;break;}
+word_array.push(i);while((word_array.length % 16)!=14)word_array.push(0);word_array.push(msg_len>>>29);word_array.push((msg_len<<3)&0x0ffffffff);for(blockstart=0;blockstart<word_array.length;blockstart+=16){for(i=0;i<16;i++)W[i]=word_array[blockstart+i];for(i=16;i<=79;i++)W[i]=rotate_left(W[i-3]^W[i-8]^W[i-14]^W[i-16],1);A=H0;B=H1;C=H2;D=H3;E=H4;for(i=0;i<=19;i++){temp=(rotate_left(A,5)+((B&C)|(~B&D))+E+W[i]+0x5A827999)&0x0ffffffff;E=D;D=C;C=rotate_left(B,30);B=A;A=temp;}
+for(i=20;i<=39;i++){temp=(rotate_left(A,5)+(B^C^D)+E+W[i]+0x6ED9EBA1)&0x0ffffffff;E=D;D=C;C=rotate_left(B,30);B=A;A=temp;}
+for(i=40;i<=59;i++){temp=(rotate_left(A,5)+((B&C)|(B&D)|(C&D))+E+W[i]+0x8F1BBCDC)&0x0ffffffff;E=D;D=C;C=rotate_left(B,30);B=A;A=temp;}
+for(i=60;i<=79;i++){temp=(rotate_left(A,5)+(B^C^D)+E+W[i]+0xCA62C1D6)&0x0ffffffff;E=D;D=C;C=rotate_left(B,30);B=A;A=temp;}
+H0=(H0+A)&0x0ffffffff;H1=(H1+B)&0x0ffffffff;H2=(H2+C)&0x0ffffffff;H3=(H3+D)&0x0ffffffff;H4=(H4+E)&0x0ffffffff;}
+var temp=cvt_hex(H0)+cvt_hex(H1)+cvt_hex(H2)+cvt_hex(H3)+cvt_hex(H4);return temp.toLowerCase();}
+
+
+
 
